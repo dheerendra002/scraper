18082e3b84429c8fd47e5f83c50294cde6cbe997
micscov2 <micscov2@gmail.com>
2016-09-03 01:56:20 -0700

    Working scraper

--- test.py
+++ test.py
@@ -24,7 +24,19 @@ def get_question_urls(section_url):
 			print(e)
 
 
-with open("data.txt", "w") as f:
-	for url in get_question_urls(""):
-		f.write(url + "\n")
+def get_related_data_to_question(ques_url):
+	html = urlopen(QUESTIONS_BASE_URL + ques_url)
+	soup = BeautifulSoup(html, "lxml")
+	question_desc = soup.find("div", "header")
+	user = soup.find("div", "member-rep-container").a.text
+	profile_url = soup.find("div", "member-rep-container").a["href"]
+	rating = soup.find("div", "member-rep-container").span.text
+	tag0 = soup.find_all("span", "t")[0].text
+	answer = soup.find("div", "answer-row answer first").find("div", "text").text
+	#user = soup.find("a", id="ct100_ct100_MC_AMC_QuestionAuthorRepInfo_MemberName")
+	print(answer)
+
+#for url in get_question_urls(""):
+#	get_related_data_to_question(url)
+get_related_data_to_question("/Questions/1121991/How-can-I-calculate-time-interval-between-date-hou")
 	
