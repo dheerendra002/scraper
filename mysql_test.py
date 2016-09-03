fdb33a48c0d74a7d0117f9bab1869151dec26ec5
micscov2 <micscov2@gmail.com>
2016-09-03 02:36:13 -0700

    Added mysql code

---
+++ mysql_test.py
@@ -0,0 +1,14 @@
+import MySQLdb
+import time
+import pdb
+
+def insert_data(question_id, user="", profile_url="", rating="", answer="", source_website="", tags="", category="", correct_answer=""):
+	sql_query = "INSERT INTO ques_ans(question_id, user, profile_url, rating, answer, source_website, tags, category, correct_answer) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s') " % (question_id, user, profile_url, rating, answer, source_website, tags, category, correct_answer)
+	db = MySQLdb.connect("localhost", "root", "pzk", "scrape")
+	cursor = db.cursor()
+	try:
+		cursor.execute(sql_query)
+		db.commit()
+	except Exception as e:
+		print(e)
+		db.rollback()
