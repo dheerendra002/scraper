b166bc411742df973130a747e0cc9cd74be0b6a4
micscov2 <micscov2@gmail.com>
2016-09-03 03:18:14 -0700

    Cleaning up

--- mysql_helper.py
+++ mysql_helper.py
@@ -4,7 +4,7 @@ import pdb
 
 def insert_data(question_id, user="", profile_url="", rating="", answer="", source_website="", tags="", category="", correct_answer=""):
 	sql_query = "INSERT INTO ques_ans(question_id, user, profile_url, rating, answer, source_website, tags, category, correct_answer) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s') " % (question_id, user, profile_url, rating, answer, source_website, tags, category, correct_answer)
-	db = MySQLdb.connect("localhost", "root", "pzk", "scrape")
+	db = MySQLdb.connect("localhost", "root", "password", "scrape")
 	cursor = db.cursor()
 	try:
 		cursor.execute(sql_query)
