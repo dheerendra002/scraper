e8983128fe2b6912ff2e982e96fea30cfab6d443
dheerendra002 <dheerendra002@gmail.com>
2016-09-06 23:51:16 +0530

    done

--- mysql_helper.py
+++ mysql_helper.py
@@ -3,7 +3,9 @@ import time
 import pdb
 
 def insert_data(question_id, user="", profile_url="", rating="", answer="", source_website="", tags="", category="", correct_answer=""):
-	sql_query = "INSERT INTO ques_ans(question_id, user, profile_url, rating, answer, source_website, tags, category, correct_answer) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s') " % (question_id, user, profile_url, rating, answer, source_website, tags, category, correct_answer)
+	sql_query = "INSERT INTO question_answer(question_short, user, profile_url, rating, answer, source_website, tags, category, \
+	 correct_answer) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s') " \
+% (question_short, user, profile_url, rating, answer, source_website, tags, category, correct_answer)
 	db = MySQLdb.connect("localhost", "root", "root", "scrape")
 	cursor = db.cursor()
 	try:
