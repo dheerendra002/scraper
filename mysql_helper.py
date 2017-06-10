21f22c1d5fc106c4cdb268f722c86f87fdc844a5
dheerendra002 <dheerendra002@gmail.com>
2017-06-10 22:53:15 +0600

    changes in mysql helper | done

--- mysql_helper.py
+++ mysql_helper.py
@@ -1,1 +1,15 @@
-4badd14a-4dfd-11e7-ae8d-9801a7a7ddd5
+import MySQLdb
+import time
+import pdb
+
+def insert_data(question_short, user="", profile_url="", rating="", answer="", source_website="", tags="", category="", correct_answer=""):
+	sql_query = "INSERT INTO question_answers(question_short, user, profile_url, rating, answer, source_website, tags, category, \
+	 correct_answer) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s') " \
+% (question_short, user, profile_url, rating, answer, source_website, tags, category, correct_answer)
+	db = MySQLdb.connect("localhost", "root", "root", "scrape")
+	cursor = db.cursor()
+	try:
+		cursor.execute(sql_query)
+		db.commit()
+	except Exception as e:
+		db.rollback()
