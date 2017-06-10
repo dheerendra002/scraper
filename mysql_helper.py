6f3fde82f00c5c0098c6a41d737919260baf7e0c
dheerendra002 <dheerendra002@gmail.com>
2017-06-10 08:18:56 +0000

    this is my first commit?

--- mysql_helper.py
+++ mysql_helper.py
@@ -1,17 +1,1 @@
-import MySQLdb
-import time
-import pdb
-
-def insert_data(question_id, user="", profile_url="", rating="", answer="", source_website="", tags="", category="", correct_answer=""):
-	sql_query = "INSERT INTO question_answer(question_short, user, profile_url, rating, answer, source_website, tags, category, \
-	 correct_answer) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s') " \
-% (question_short, user, profile_url, rating, answer, source_website, tags, category, correct_answer)
-	db = MySQLdb.connect("localhost", "root", "root", "scrape")
-	cursor = db.cursor()
-	try:
-		cursor.execute(sql_query)
-		db.commit()
-		print("inserted")
-	except Exception as e:
-		print(e)
-		db.rollback()
+07aca661-4dfd-11e7-915e-9801a7a7ddd5
